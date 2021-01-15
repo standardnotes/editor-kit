4c5c1dd2edc21f8dcad457e396b29b5e6bb36e91
Johnny Almonte <johnny243@users.noreply.github.com>
2021-01-15 00:36:25 -0400

    fix: lint webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -2,40 +2,40 @@ const path = require('path');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-    mode: "production",
-    entry: {
-      "editor-kit": "./src/index.js",
-      "editor-kit.min": "./src/index.js",
-    },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: './[name].js',
-        sourceMapFilename: '[name].js.map',
-        library: 'EditorKit',
-        libraryTarget: 'umd',
-        umdNamedDefine: true
-    },
-    externals: {
-      'filesafe-js': 'filesafe-js'
-    },
-    module: {
-      rules: [
-        {
-          test: /\.js$/,
-          loader: 'babel-loader',
-        },
-      ]
-    },
-    stats: {
-        colors: true
-    },
-    plugins: [
-      new CopyWebpackPlugin([
-        {
-          from: './node_modules/filesafe-js/dist/filesafe-js/EncryptionWorker.js',
-          to: 'filesafe-js/EncryptionWorker.js'
-        },
-      ]),
-    ],
-    devtool: 'source-map'
+  mode: "production",
+  entry: {
+    "editor-kit": "./src/index.js",
+    "editor-kit.min": "./src/index.js",
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: './[name].js',
+    sourceMapFilename: '[name].js.map',
+    library: 'EditorKit',
+    libraryTarget: 'umd',
+    umdNamedDefine: true
+  },
+  externals: {
+    'filesafe-js': 'filesafe-js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+      },
+    ]
+  },
+  stats: {
+    colors: true
+  },
+  plugins: [
+    new CopyWebpackPlugin([
+      {
+        from: './node_modules/filesafe-js/dist/filesafe-js/EncryptionWorker.js',
+        to: 'filesafe-js/EncryptionWorker.js'
+      },
+    ]),
+  ],
+  devtool: 'source-map'
 };
